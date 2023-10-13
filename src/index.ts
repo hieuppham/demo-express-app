3c297ee77db5681605aafb737b24b72347faef9a
HieuPT <hieupt@vissoft.vn>
2023-10-13 09:51:49 +0700

    Health check

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import express, { Express } from "express";
 const app: Express = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>Home page</h1>")
+    res.send("<h1>Home page</h1>");
 });
 
 app.get("/about", (req, res) => {
-    res.send("<h1>About page</h1>")
+    res.send("<h1>About page</h1>");
+});
+
+app.get("/members", (req, res) => {
+    res.send("<h1>HieuPT: phamtrunghieu6d@gmail.com</h1>");
 });
 
 const PORT = process.env.PORT || 8099;
