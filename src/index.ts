0c05f572d5b82c831781c3e32c180665bc81eaa1
HieuPT <hieupt@vissoft.vn>
2023-10-13 10:07:51 +0700

    Remove endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,6 @@ app.get("/about", (req, res) => {
     res.send("<h1>About page</h1>");
 });
 
-app.get("/members", (req, res) => {
-    res.send("<h1>HieuPT: phamtrunghieu6d@gmail.com</h1>");
-});
-
 const PORT = process.env.PORT || 8099;
 
 const server = app.listen(PORT, () => {
