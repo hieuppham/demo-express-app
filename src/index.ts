5165f4235b4fd3628b2ca10cfbc83ffd763a3a4e
HieuPT <hieupt@vissoft.vn>
2025-03-11 10:59:58 +0700

    add log

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import express, { Express, Request, Response } from "express";
+import winston from "winston";
 
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.timestamp({format: "YYYY-MM-DD HH:mm:ss"}),
+                winston.format.colorize({level: true, all: true})
+            )
+        })
+    ]
+});
 const app: Express = express();
 
 app.get('/ping', (req: Request, res: Response) => {
+    logger.info("Received ping request");
     res.json({ message: 'Pong!' });
 });
 
 app.get('/whoiam', (req: Request, res: Response) => {
+    logger.info("Received whoiam request");
     res.json({ message: 'hieuppham' });
 });
 
 app.get('/check', (req: Request, res: Response) => {
+    logger.info("Received check");
     res.json({ message: 'ok 👍👍👍👍👍👍' });
 });
 
